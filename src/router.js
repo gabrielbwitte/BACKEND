0e5b367b2cb8304e450aa65b75075bd086f6755b
gabrielbwitte <wittegabrielb@gmail.com>
2024-09-06 20:16:39 -0400

    1

--- src/router.js
+++ src/router.js
@@ -2,6 +2,6 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => res.status(200).send('Ola'));
+//router.get('/', (req, res) => res.status(200).send('Ola'));
 
 module.exports = router;

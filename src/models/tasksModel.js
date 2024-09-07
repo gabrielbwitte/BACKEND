c6c6ec6bb6e321026d3ac1778b938f9e34a8502f
gabrielbwitte <wittegabrielb@gmail.com>
2024-09-07 16:40:07 -0400

    Dia 7, está ficando bom

--- src/models/tasksModel.js
+++ src/models/tasksModel.js
@@ -1,7 +1,23 @@
-const getAll = () => {
+const connection = require('./connection');
 
+const getAll = async () => {
+    const [tasks] = await connection.execute('SELECT * FROM tasks');
+    return tasks;
+};
+
+const createTask = async (task) => {
+    const { title } = task;
+
+    const dateUTC = new Date(Date.now()).toUTCString();
+
+    const query = 'INSERT INTO tasks(title, status, created_at) VALUES (?, ?, ?)';
+    
+    const [ createdTask ] = await connection.execute(query, [title, 'pendente', dateUTC]);
+
+    return {inseertId: createdTask.insertId};
 };
 
 module.exports = {
-    getAll
+    getAll,
+    createTask
 };
--- src/router.js
+++ src/router.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const tasksControllers = require('./controllers/tasksControllers');
 
 const router = express.Router();
 
-//router.get('/', (req, res) => res.status(200).send('Ola'));
+router.get('/tasks', tasksControllers.getAll);
+router.post('/tasks', tasksControllers.createTask);
+
 
 module.exports = router;

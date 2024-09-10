35d7f47bde572c9d996674370a089424d6885a4a
gabrielbwitte <wittegabrielb@gmail.com>
2024-09-09 20:40:47 -0400

    Finalizado a primeira versão

---
+++ src/middlewares/tasksMiddlewares.js
@@ -0,0 +1,26 @@
+const validateFieldsTitle = (request, response, next) => {
+    const { body } = request;
+    if(body.title === undefined) {
+        return response.status(400).json({ message: 'The field "title" is required' });
+    };
+    if (body.title === '') {
+        return response.status(400).json({ message: 'title cannot be empty'});
+    };
+    next();
+};
+
+const validateFieldsStatus = (request, response, next) => {
+    const { body } = request;
+    if(body.status === undefined) {
+        return response.status(400).json({ message: 'The field "status" is required' });
+    };
+    if (body.status === '') {
+        return response.status(400).json({ message: 'status cannot be empty'});
+    };
+    next();
+};
+
+module.exports = {
+    validateFieldsTitle,
+    validateFieldsStatus
+};
--- src/router.js
+++ src/router.js
@@ -2,9 +2,15 @@ const express = require('express');
 const tasksControllers = require('./controllers/tasksControllers');
 
 const router = express.Router();
+const tasksMiddleware = require('./middlewares/tasksMiddlewares');
 
 router.get('/tasks', tasksControllers.getAll);
-router.post('/tasks', tasksControllers.createTask);
+router.post('/tasks',tasksMiddleware.validateFieldsTitle, tasksControllers.createTask);
+router.delete('/tasks/:id', tasksControllers.deleteTask);
+router.put('/tasks/:id',
+    tasksMiddleware.validateFieldsTitle,
+    tasksMiddleware.validateFieldsStatus,
+    tasksControllers.updateTask);
 
 
 module.exports = router;
